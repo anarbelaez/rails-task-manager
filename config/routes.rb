e65487e613cf33210e438fd9034870259de4bdde
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-09 15:48:02 -0500

    Added style and I refactored the code - before_action, _form and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks' => 'tasks#index'
-  get 'tasks/new' => 'tasks#new'
-  post 'tasks' => 'tasks#create'
-  get 'tasks/:id' => 'tasks#show', as: :task
-  get 'tasks/:id/edit' => 'tasks#edit', as: :task_edit
-  patch 'tasks/:id' => 'tasks#update'
-  delete 'tasks/:id' => 'tasks#destroy'
+
+  resources :tasks
+
+  # get 'tasks' => 'tasks#index'
+  # get 'tasks/new' => 'tasks#new'
+  # post 'tasks' => 'tasks#create'
+  # get 'tasks/:id' => 'tasks#show', as: :task
+  # get 'tasks/:id/edit' => 'tasks#edit', as: :task_edit
+  # patch 'tasks/:id' => 'tasks#update'
+  # delete 'tasks/:id' => 'tasks#destroy'
 end
