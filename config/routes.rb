6f8e6f138d972d004957bff0aafac9d81e78d0f4
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-09 15:01:56 -0500

    Basic CRUD ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # root "articles#index"
-
-  get 'tasks' => 'tasks#index', as: :index
-  get 'tasks/:id' => 'tasks#show', as: :show
+  get 'tasks' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+  get 'tasks/:id' => 'tasks#show', as: :task
+  get 'tasks/:id/edit' => 'tasks#edit', as: :task_edit
+  patch 'tasks/:id' => 'tasks#update'
+  delete 'tasks/:id' => 'tasks#destroy'
 end
