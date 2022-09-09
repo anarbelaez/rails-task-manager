297bddc5cf0f10972cedcf4fdae3bce3f504e0e0
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-09 11:59:28 -0500

    Read actions ok - basic behavior

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'tasks' => 'tasks#index', as: :index
+  get 'tasks/:id' => 'tasks#show', as: :show
 end
